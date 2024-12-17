68e3129d5a0ca0cfaf1f213d67e512fccbe4152c
jazeem <jazeemmp404@gmail.com>
2024-12-17 16:45:02 +0530

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Header/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Outlet/>
     </>
   );
 }
