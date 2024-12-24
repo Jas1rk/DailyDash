e10b5402f2a3039e81923e2b31800c92f5194f03
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-12-24 17:03:27 +0530

    jsonwebtoken and enums for status code

---
+++ Backend/src/Enums/httpStatusCode.ts
@@ -0,0 +1,15 @@
+enum httpStatus_Code {
+    OK = 200,
+    NoChange = 301,
+    TaskFailed = 304,
+    BadRequest = 400,
+    Unauthorized = 401,
+    NoAccess = 403,
+    NotFound = 404,
+    Conflict = 409,
+    Expired = 410,
+    InternalServerError = 500,
+    ServiceUnavailable = 503,
+}
+
+export default httpStatus_Code
---
+++ Backend/src/Utils/jwt.ts
@@ -0,0 +1,75 @@
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import { Request, Response, NextFunction } from 'express'
+import httpStatus_Code from '../Enums/httpStatusCode'
+
+
+dotenv.config()
+
+interface TokenPayload extends JwtPayload {
+    userid: string
+}
+
+const accessKey = process.env.JWT_ACCESS_SECRET_KEY as string
+const refreshKey = process.env.JWT_REFRESH_SECRET_KEY as string
+
+export const createAccessToken = (userid: string) => {
+    const accessToken = jwt.sign({ userid }, accessKey, { expiresIn: "1h" })
+    return accessToken
+}
+
+export const createRefreshToken = (userid: string) => {
+    const refreshToken = jwt.sign({ userid }, refreshKey, { expiresIn: "7d" })
+    return refreshToken
+}
+
+
+export const verifyAccessToken = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const accessToken = req.cookies["accessToken"]
+        if (!accessToken) {
+            return res.status(httpStatus_Code.Unauthorized).json({ message: "No Access Token Provided" })
+        }
+
+        jwt.verify(accessToken, accessKey, (err: Error | null, decoded: string | JwtPayload | undefined) => {
+            if (err) {
+                return verifyRefreshToken(req, res, next)
+            }
+            const payload = decoded as TokenPayload
+            req.userid = payload.userid
+            next()
+        })
+
+    } catch (error) {
+        return res.status(httpStatus_Code.InternalServerError).json({ message: "Internal Server Error" })
+    }
+}
+
+export const verifyRefreshToken = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const refreshToken = req.cookies["refreshToken"]
+        if (!refreshToken) {
+            return res.status(401).json({ message: "No Refresh Token Provided" })
+        }
+
+        jwt.verify(refreshToken, refreshKey, (err: Error | null, decoded: string | JwtPayload | undefined) => {
+            if (err) {
+                return res.status(httpStatus_Code.Unauthorized).json({ message: "Invalid Refresh Token" })
+            }
+
+            const newToken = createAccessToken(decoded.userid)
+            res.cookie("accessToken", newToken, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'strict',
+                maxAge: 1 * 60 * 60 * 1000,
+            })
+            const payload = decoded as TokenPayload
+            req.userid = payload.userid
+            next()
+        })
+
+    } catch (error) {
+        res.status(httpStatus_Code.InternalServerError).json({ message: "Internal Server Error" })
+    }
+}
