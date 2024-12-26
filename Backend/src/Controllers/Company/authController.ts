8beb0e822713d2a71fdd59badf7a79656e10598f
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-12-27 00:30:34 +0530

    some backend configuration

---
+++ Backend/src/Controllers/Company/authController.ts
@@ -0,0 +1,10 @@
+import { Request, Response, } from "express";
+import httpStatus_Code from "../../Enums/httpStatsuCode";
+
+export const registerCompany = async (req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
+        res.status(httpStatus_Code.InternalServerError).json({ message: "Internal Server Error" })
+    }
+}
