9f6bd1745f42002e7d089ce499cce92ac8b754ca
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2025-03-03 23:32:31 +0530

    minor changes

--- Backend/src/Controllers/Employ/authController.ts
+++ Backend/src/Controllers/Employ/authController.ts
@@ -3,20 +3,24 @@ import httpStatus_Code from "../../Enums/httpStatusCode";
 import Employ from '../../Schema/employSchema'
 import dotenv from 'dotenv'
 dotenv.config()
+import { LoginTicket, OAuth2Client } from "google-auth-library";
 
 const googleClientId = process.env.GOOGLE_CLIENT_ID
+const client = new OAuth2Client(googleClientId)
+
+
 
 
 export const registerEmploy = async (req: Request, res: Response): Promise<void> => {
     try {
-        console.log("there is a man ",req.body)
+        console.log("there is a man ", req.body)
         const { employEmail } = req.body
         const isExistEmail = await Employ.findOne({ email: employEmail })
         if (isExistEmail) {
             res.status(httpStatus_Code.NoAccess).json({ message: "The Email you entered is already exist" })
             return
         }
-        
+
 
 
     } catch (error) {
@@ -36,8 +40,26 @@ export const employAuthOtp = async (req: Request, res: Response): Promise<void> 
 
 export const googleAuthentication = async (req: Request, res: Response): Promise<void> => {
     try {
-        console.log(req.body)
-        console.log("this controller is working good as expected so my team is gonna work here with nod js and express js mongodb, jwt , nodemailer, middleware ")
+        const { token } = req.body
+        if (!token) {
+            res.status(httpStatus_Code.Unauthorized).json({ message: "Failed to login, Token is not provided" })
+            return
+        }
+        const ticket: LoginTicket = await client.verifyIdToken({
+            idToken: token,
+            audience: googleClientId
+        })
+        const payload = ticket.getPayload()
+        if (!payload) {
+            res.status(httpStatus_Code.Unauthorized).json({ message: "Login failed. Failed to retrieve user data" })
+            return
+        }
+        const { email } = payload
+        if (!email) {
+            res.status(httpStatus_Code.Unauthorized).json({ message: "Email is not provided while google authentication." })
+            return
+        }
+        
     } catch (error) {
         res.status(httpStatus_Code.ServiceUnavailable).json({ message: "Service Unavailable" })
     }
