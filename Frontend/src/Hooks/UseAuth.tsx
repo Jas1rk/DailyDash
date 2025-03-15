dcf67a7671b470bb19b4be515da6cf38e2be5894
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2025-03-15 14:20:51 +0530

    minor ui changes

--- Frontend/src/Hooks/UseAuth.tsx
+++ Frontend/src/Hooks/UseAuth.tsx
@@ -1,20 +1,23 @@
 import axios from "axios";
 import { backendUrl } from "../Service/BackendUrl";
 import { useAuth } from "../Context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const useAuthHandler = () => {
-  const {setEmployData}:any = useAuth()
+  const navigate = useNavigate();
+  const { setEmployData }: any = useAuth();
   const googleLogin = async (credential: string | undefined): Promise<void> => {
     console.log("Google Credential Received:", credential);
     try {
-      const {data} = await axios.post(
+      const { data } = await axios.post(
         `${backendUrl}/employ/google/auth`,
         { token: credential },
         { withCredentials: true }
       );
       console.log(data, "here is the damn response");
-  
-      setEmployData(data.employ)
+
+      setEmployData(data.employ);
+      if (data) navigate("/");
     } catch (error) {
       console.log(error, "error is here");
     }
