0d2e63e6ed5be007ebe6dde2083145685e9d49be
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2025-03-21 11:34:05 +0530

    minor changes

--- Backend/src/Config/bcrypt.ts
+++ Backend/src/Config/bcrypt.ts
@@ -1,10 +1,10 @@
 import bcrypt from 'bcrypt'
 
-export const hashPassword = async(password:string):Promise<string> => {
-   return await bcrypt.hash(password,10)
+export const hashPassword = async (password: string): Promise<string> => {
+    return await bcrypt.hash(password, 10)
 }
 
 
-export const comparePassword = async(password:string):Promise<string> => {
-    return await bcrypt.compare(password)
+export const comparePassword = async (password: string, hashPassword: string): Promise<boolean> => {
+    return await bcrypt.compare(password, hashPassword)
 }
